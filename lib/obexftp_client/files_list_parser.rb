9866da9b39f222b841e6224366f9bc99f0984e3b
Jonathan Pares <jonathan.pares@gmail.com>
2012-01-18 21:21:10 +0800

    Wrote the list returned by obex parser.

---
+++ lib/obexftp_client/files_list_parser.rb
@@ -0,0 +1,17 @@
+module ObexftpClient
+	class FilesListParser
+   
+    def self.parse_ls(obexftp_ls_result, type)
+      doc = Nokogiri::XML(obexftp_ls_result)
+      
+      case type
+      when :file then
+        return doc.xpath("//file")
+      when :folder then
+        return doc.xpath("//folder")
+      else
+        return []
+      end
+    end
+  end
+end
